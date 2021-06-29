c34871f51a0b02e108c809183cbff977b3e4c4ec
Pedro Crespo <pedbcrespo@gmail.com>
2021-06-29 11:06:05 -0300

    ajeitei algumas funcoes api

--- API/api.py
+++ API/api.py
@@ -10,6 +10,39 @@ CORS(app)
 class Conexao:
     bd = BancoDados()
 
+# Classes com busca de valor especifico
+class Produto(Resource, Conexao):
+    def get(self, id):
+        dados = "produto.id, produto.nome as produto, categoria.nome  as categoria ,produto.preco, produto.caminhoImagem"
+        complemento = f"inner join categoria where produto.categoriaProduto = categoria.id and produto.id = {id}"
+        lista_resposta = self.bd.getTabela("produto", dados, complemento)
+        return lista_resposta[0]
+
+class Categoria(Resource, Conexao):
+    def get(self, id):
+        lista_resposta = self.bd.getTabela("categoria", "*", f"WHERE id = {id}")
+        return lista_resposta[0]
+
+class Cliente(Resource, Conexao):
+    def get(self, id):
+        lista_resposta = self.bd.getTabela("cliente", "*", f"WHERE id = {id}")
+        return lista_resposta[0]
+
+class Carrinho(Resource, Conexao):
+    def get(self, id):
+        dados = "carrinho.id, cliente.nome as cliente, carrinho.idCliente ,produto.nome as produto, carrinho.idProduto, produto.preco"
+        complemento = f"inner join cliente inner join produto where cliente.id = carrinho.idCliente and produto.id = carrinho.idProduto and carrinho.id = {id}"
+        lista_resposta = self.bd.getTabela("carrinho", dados, complemento)
+        return lista_resposta[0]
+        
+# Classes com busca de todos os valores registrados 
+class Produtos(Resource, Conexao):
+    def get(self):
+        return self.bd.getTabela("produto", "*")
+    
+class Categorias(Resource, Conexao):
+    def get(self):
+        return self.bd.getTabela("categoria", "*")
 
 if __name__ == '__main__':
     app.run(debug=True)
