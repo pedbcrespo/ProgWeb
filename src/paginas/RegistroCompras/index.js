d7bbac2d5ee94b5a5b34641805cc6c8ad5a46326
Pedro Crespo <pedbcrespo@gmail.com>
2021-08-20 17:18:42 -0300

    estilisando alguns componentese e paginas

--- src/paginas/RegistroCompras/index.js
+++ src/paginas/RegistroCompras/index.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { getTodasCompras, getTodasInfoCliente } from '../../server/api';
-// import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
-import InfoCliente from '../../componentes/InfoCliente';
 import { Table } from 'react-bootstrap';
 
 export default function RegistroCompras() {
@@ -18,28 +16,6 @@ export default function RegistroCompras() {
     return (
         <section>
             <h2>Compras realizadas</h2>
-            {/* <div className="campTabela">
-                <TableContainer >
-                    <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Usuario</TableCell>
-                                <TableCell align="right">Produto</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {listaCompras.map((row) => (
-                                <TableRow key={row.id}>
-                                    <TableCell component="th" scope="row">
-                                        {row.idCliente}
-                                    </TableCell>
-                                    <TableCell align="right">{row.idProduto}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div> */}
             <Table striped bordered hover variant="dark" className="tabela">
                 <thead>
                     <tr>
@@ -59,23 +35,6 @@ export default function RegistroCompras() {
 
             <h2>Informações Clientes</h2>
             <div className="campTabela">
-                {/* <TableContainer >
-                    <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell align="right">Email</TableCell>
-                                <TableCell align="right">Endereço</TableCell>
-                                <TableCell align="right">CEP</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {listaInfoClientes.map((row) => (
-                                <InfoCliente cliente={row} />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer> */}
 
                 <Table striped bordered hover variant="dark" className="tabela">
                     <thead>
