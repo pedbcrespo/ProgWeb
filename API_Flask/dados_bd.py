10e0909b9d1934367b357d15beeefaa29ce89cd5
Pedro Crespo <pedbcrespo@gmail.com>
2021-08-31 22:15:33 -0300

    preparando para subir

--- API_Flask/dados_bd.py
+++ API_Flask/dados_bd.py
@@ -1,4 +1,6 @@
-usuario = 'root'
-senha = ''
-host = 'localhost'
-bancoDados = 'progweb'
+# mysql://b7bff7d15f2ca7:561d309b@us-cdbr-east-04.cleardb.com/heroku_adbe231665a19ea?reconnect=true
+
+usuario = 'b7bff7d15f2ca7'
+senha = '561d309b'
+host = 'us-cdbr-east-04.cleardb.com'
+bancoDados = 'heroku_adbe231665a19ea'
