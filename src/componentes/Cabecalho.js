984f1e3a91718b375da87b0e54498092a7d3db52
Pedro Crespo <pedbcrespo@gmail.com>
2021-09-17 17:42:03 -0300

    ajeitando detalhes do carrinho

--- src/componentes/Cabecalho.js
+++ src/componentes/Cabecalho.js
@@ -12,7 +12,7 @@ export default function Cabecalho() {
     return acesso ? <CabecalhoAdm /> : (
         <>
             <header className="Cabecalho">
-                <h1><img src={logo} alt="logo da pagina"/></h1>
+                <h1><img src={logo} alt="logo da pagina" /></h1>
             </header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
@@ -38,6 +38,10 @@ export default function Cabecalho() {
                                 </ul>
                             </li>
                         </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button class ="btn btn-outline-success" type ="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </nav>
